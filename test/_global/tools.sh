cc38abb6c661f032dc3c6f8bbfdc1a9f9897ef1b
Lyon Till <158992+ljtill@users.noreply.github.com>
2023-07-21 23:47:45 +0100

    feat: add clusterctl tool (#12)

--- test/_global/tools.sh
+++ test/_global/tools.sh
@@ -7,6 +7,7 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 check "version" argocd version --client --output json
+check "version" clusterctl version
 check "version" flux version --client --output json
 check "version" helm version
 check "version" kubectl version --client=true --output=json
