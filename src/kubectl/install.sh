43dca47c177fb81cc664d0ff0123a4a02b9cff74
Lyon Till <158992+ljtill@users.noreply.github.com>
2023-07-21 18:14:41 +0100

    feat: add kubectl tool (#4)

---
+++ src/kubectl/install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e
+
+check() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+            echo "Running apt update..."
+            apt update -y
+        fi
+        apt -y install --no-install-recommends "$@"
+    fi
+}
+
+export DEBIAN_FRONTEND=noninteractive
+
+check curl ca-certificates
+
+install() {
+    curl -Lo ./kubectl https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
+    chmod +x ./kubectl
+    chown root:root ./kubectl
+    mv ./kubectl /usr/local/bin/kubectl
+}
+
+echo "Activating feature 'kubectl'"
+
+install
