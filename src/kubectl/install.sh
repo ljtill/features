e2d34990b9d20b09f2b2d914b6ce6ad0d0b784da
Lyon Till <lyon.till@microsoft.com>
2023-07-21 17:58:17 +0100

    feat: add kubectl tool

--- src/kubectl/install.sh
+++ src/kubectl/install.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
 set -e
 
-echo "Activating feature 'kubectl'"
-curl -Lo ./kubectl https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
-chmod +x ./kubectl
-chown root:root ./kubectl
-mv ./kubectl /usr/local/bin/kubectl
+check() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+            echo "Running apt update..."
+            apt update -y
+        fi
+        apt -y install --no-install-recommends "$@"
+    fi
+}
+
+check curl
+
+install() {
+    echo "Activating feature 'kubectl'"
+    curl -Lo ./kubectl https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
+    chmod +x ./kubectl
+    chown root:root ./kubectl
+    mv ./kubectl /usr/local/bin/kubectl
+}
+
+install
