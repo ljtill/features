c8284b971ebfa86bf14c93e553306b21dd82235c
Lyon Till <158992+ljtill@users.noreply.github.com>
2023-07-26 13:16:40 +0100

    feat: add kubebuilder tool (#37)

---
+++ src/kubebuilder/install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -e
+
+cd /tmp
+
+check() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+            echo "Running apt update..."
+            apt update -y
+        fi
+        apt -y install --no-install-recommends "$@"
+    fi
+}
+
+export DEBIAN_FRONTEND=noninteractive
+
+check curl ca-certificates
+
+install() {
+    curl -Lo ./kubebuilder https://github.com/kubernetes-sigs/kubebuilder/releases/latest/download/kubebuilder_linux_amd64
+    chmod +x ./kubebuilder
+    chown root:root ./kubebuilder
+    mv ./kubebuilder /usr/local/bin/kubebuilder
+}
+
+echo "Activating feature 'kubebuilder'"
+
+install
