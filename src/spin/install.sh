a1cc675d4ae41b409c6ab3a2203f64ba9f7efd17
Lyon Till <lyon.till@microsoft.com>
2023-07-26 12:19:11 +0100

    chore: update feature manifest

--- src/spin/install.sh
+++ src/spin/install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+cd /tmp
+
 check() {
     if ! dpkg -s "$@" > /dev/null 2>&1; then
         if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
@@ -16,7 +18,10 @@ export DEBIAN_FRONTEND=noninteractive
 check curl ca-certificates git
 
 install() {
-    curl https://developer.fermyon.com/downloads/install.sh | bash
+    version=$(curl -sL https://api.github.com/repos/fermyon/spin/releases/latest | jq -r ".tag_name" | sed 's/v//')
+    curl -Lo ./spin-linux-amd64.tar.gz https://github.com/fermyon/spin/releases/download/v"$version"/spin-v"$version"-linux-amd64.tar.gz
+    tar -zxof ./spin-linux-amd64.tar.gz
+    rm -f ./spin-linux-amd64.tar.gz
     chmod +x ./spin
     chown root:root ./spin
     mv ./spin /usr/local/bin/spin
