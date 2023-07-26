362f1b3b3c5cc315f927c93df241c3d5eb60b819
Lyon Till <lyon.till@microsoft.com>
2023-07-26 12:16:53 +0100

    chore: update feature manifest

--- src/flux/install.sh
+++ src/flux/install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+cd /tmp
+
 check() {
     if ! dpkg -s "$@" > /dev/null 2>&1; then
         if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
@@ -17,9 +19,9 @@ check curl ca-certificates jq
 
 install() {
     version=$(curl -sL https://api.github.com/repos/fluxcd/flux2/releases/latest | jq -r ".tag_name" | sed 's/v//')
-    curl -Lo ./flux_"$version"_linux_amd64.tar.gz https://github.com/fluxcd/flux2/releases/download/v"$version"/flux_"$version"_linux_amd64.tar.gz
-    tar -zxof ./flux_"$version"_linux_amd64.tar.gz
-    rm -f ./flux_"$version"_linux_amd64.tar.gz
+    curl -Lo ./flux_linux_amd64.tar.gz https://github.com/fluxcd/flux2/releases/download/v"$version"/flux_"$version"_linux_amd64.tar.gz
+    tar -zxof ./flux_linux_amd64.tar.gz
+    rm -f ./flux_linux_amd64.tar.gz
     chmod +x ./flux
     chown root:root ./flux
     mv ./flux /usr/local/bin/flux
