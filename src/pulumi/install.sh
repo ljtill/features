2d36b77c91ccb4b0ba30022671fae09cd22394a5
Lyon Till <lyon.till@microsoft.com>
2023-07-26 12:18:49 +0100

    chore: update feature manifest

--- src/pulumi/install.sh
+++ src/pulumi/install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+cd /tmp
+
 check() {
     if ! dpkg -s "$@" > /dev/null 2>&1; then
         if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
@@ -17,9 +19,9 @@ check curl ca-certificates jq
 
 install() {
     version=$(curl -sL https://api.github.com/repos/pulumi/pulumi/releases/latest | jq -r ".tag_name" | sed 's/v//')
-    curl -Lo ./pulumi-v"$version"-linux-x64.tar.gz https://get.pulumi.com/releases/sdk/pulumi-v"$version"-linux-x64.tar.gz
-    tar -zxof ./pulumi-v"$version"-linux-x64.tar.gz
-    rm -f ./pulumi-v"$version"-linux-x64.tar.gz
+    curl -Lo ./pulumi-linux-x64.tar.gz https://get.pulumi.com/releases/sdk/pulumi-v"$version"-linux-x64.tar.gz
+    tar -zxof ./pulumi-linux-x64.tar.gz
+    rm -f ./pulumi-linux-x64.tar.gz
     chmod +x ./pulumi/pulumi
     chown root:root ./pulumi/pulumi
     mv ./pulumi/pulumi /usr/local/bin/pulumi
