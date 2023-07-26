ad56235d4261944037dc9b8e68cd41fbcafc4750
Lyon Till <lyon.till@microsoft.com>
2023-07-26 11:31:16 +0100

    feat: add kind tool

---
+++ src/kind/install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e
+
+check() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+            echo "Running apt update..."
+            apt update -y
+        fi
+        apt -y install --no-install-recommends "$@"
+    fi
+}
+
+export DEBIAN_FRONTEND=noninteractive
+
+check curl ca-certificates
+
+install() {
+    curl -Lo ./kind https://github.com/kubernetes-sigs/kind/releases/download/v0.20.0/kind-linux-amd64
+    chmod +x ./kind
+    chown root:root ./kind
+    mv ./kind /usr/local/bin/kind
+}
+
+echo "Activating feature 'kind'"
+
+install
