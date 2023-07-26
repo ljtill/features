91b8592225bee0561c81c40dea15861087a6ee77
Lyon Till <lyon.till@microsoft.com>
2023-07-26 12:16:18 +0100

    chore: update feature manifest

--- src/clusterctl/install.sh
+++ src/clusterctl/install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+cd /tmp
+
 check() {
     if ! dpkg -s "$@" > /dev/null 2>&1; then
         if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
@@ -16,8 +18,7 @@ export DEBIAN_FRONTEND=noninteractive
 check curl ca-certificates jq
 
 install() {
-    version=$(curl -sL https://api.github.com/repos/kubernetes-sigs/cluster-api/releases/latest | jq -r ".tag_name" | sed 's/v//')
-    curl -Lo ./clusterctl https://github.com/kubernetes-sigs/cluster-api/releases/download/v"$version"/clusterctl-linux-amd64
+    curl -Lo ./clusterctl https://github.com/kubernetes-sigs/cluster-api/releases/latest/download/clusterctl-linux-amd64
     chmod +x ./clusterctl
     chown root:root ./clusterctl
     mv ./clusterctl /usr/local/bin/clusterctl
