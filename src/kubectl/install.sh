11d9ec19ede1150618d73ef03d0b3a660dfe9f36
Lyon Till <lyon.till@microsoft.com>
2023-08-27 21:13:11 +0100

    feat: update kubectl tool

--- src/kubectl/install.sh
+++ src/kubectl/install.sh
@@ -36,7 +36,7 @@ options() {
     if [ "${NODESHELL}" = "true" ]; then
         curl -Lo ./kubectl-node_shell https://github.com/kvaps/kubectl-node-shell/raw/master/kubectl-node_shell
         chmod +x ./kubectl-node_shell
-        chmod root:root ./kubectl-node_shell
+        chown root:root ./kubectl-node_shell
         mv ./kubectl-node_shell /usr/local/bin/kubectl-node_shell
     fi
 }
