ed5d390dfbc8b6d95694400841d609d6432e7c9c
Lyon Till <158992+ljtill@users.noreply.github.com>
2023-08-27 21:15:48 +0100

    feat: update kubectl tool (#46)

--- src/kubectl/install.sh
+++ src/kubectl/install.sh
@@ -33,6 +33,12 @@ options() {
         chown root:root ./bin/linux_amd64/kubelogin
         mv ./bin/linux_amd64/kubelogin /usr/local/bin/kubelogin
     fi
+    if [ "${NODESHELL}" = "true" ]; then
+        curl -Lo ./kubectl-node_shell https://github.com/kvaps/kubectl-node-shell/raw/master/kubectl-node_shell
+        chmod +x ./kubectl-node_shell
+        chown root:root ./kubectl-node_shell
+        mv ./kubectl-node_shell /usr/local/bin/kubectl-node_shell
+    fi
 }
 
 echo "Activating feature 'kubectl'"
