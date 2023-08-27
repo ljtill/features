c07e5ebe7d7c1af9b3dca25e13eb2dbb1164a720
Lyon Till <lyon.till@microsoft.com>
2023-08-27 23:34:01 +0100

    feat: update kubectl tool

--- test/kubectl/krew.sh
+++ test/kubectl/krew.sh
@@ -12,7 +12,7 @@ check "vscode" whoami | grep vscode
 check "kubectl" kubectl version --client=true --output=json
 
 # Options-specific tests
-check "krew" "KREW_ROOT"/kubectl-krew version
+check "krew" "$KREW_ROOT"/kubectl-krew version
 
 # Report result
 reportResults
