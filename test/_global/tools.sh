419c5b0eb12f40402b64f82526e3ffa3bb8c31e9
Lyon Till <158992+ljtill@users.noreply.github.com>
2023-10-05 18:31:37 +0100

    feat: add calicoctl tool (#85)

--- test/_global/tools.sh
+++ test/_global/tools.sh
@@ -7,6 +7,7 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 check "version" argocd version --client --output json
+check "version" calicoctl version
 check "version" clusterctl version
 check "version" flux version --client --output json
 check "version" func version
---
+++ test/calicoctl/version.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+# Import test library
+source dev-container-features-test-lib
+
+# Check the user
+check "vscode" whoami | grep vscode
+
+# Options-specific tests
+check "version" calicoctl version | grep "v3.26.2"
+
+# Report result
+reportResults
