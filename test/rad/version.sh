16aa6a079362d5e26141c663adeab967afaf8321
Lyon Till <158992+ljtill@users.noreply.github.com>
2023-10-19 09:36:27 +0100

    feat: add rad tool (#89)

---
+++ test/rad/version.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+# Import test library
+source dev-container-features-test-lib
+
+# Check the user
+check "vscode" whoami | grep vscode
+
+# Options-specific tests
+check "version" rad version | grep "v0.26.6"
+
+# Report result
+reportResults
