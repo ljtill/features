423a01ec95064d80fba705478f4c90d294cd87dc
Lyon Till <lyon.till@microsoft.com>
2023-10-19 09:23:58 +0100

    feat: add rad tool

---
+++ src/rad/README.md
@@ -0,0 +1,30 @@
+
+# Pulumi (pulumi)
+
+Command line tool (pulumi)
+
+## Example Usage
+
+```json
+"features": {
+    "ghcr.io/ljtill/features/pulumi:1": {}
+}
+```
+
+## Options
+
+| Options Id | Description | Type | Default Value |
+|-----|-----|-----|-----|
+| version | Select version of pulumi | string | latest |
+
+## Customizations
+
+### VS Code Extensions
+
+- `pulumi.pulumi-lsp-client`
+
+
+
+---
+
+_Note: This file was auto-generated from the [devcontainer-feature.json](https://github.com/ljtill/features/blob/main/src/pulumi/devcontainer-feature.json).  Add additional notes to a `NOTES.md`._
---
+++ src/rad/install.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+set -e
+
+cd "$(mktemp -d)"
+
+check() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+            echo "Running apt update..."
+            apt update -y
+        fi
+        apt -y install --no-install-recommends "$@"
+    fi
+}
+
+export DEBIAN_FRONTEND=noninteractive
+
+check curl ca-certificates jq
+
+version() {
+    if [ "${VERSION}" = "latest" ]; then
+        export VERSION=$(curl -sL https://api.github.com/repos/radius-project/radius/releases/latest | jq -r ".tag_name" | sed 's/v//')
+    else
+        export VERSION=$(echo ${VERSION} | sed 's/v//')
+    fi
+}
+
+download() {
+    curl -Lo ./rad https://github.com/radius-project/radius/releases/download/v"${VERSION}"/rad_linux_amd64
+}
+
+install() {
+    chmod +x ./rad
+    chown root:root ./rad
+    mv ./rad /usr/local/bin/rad
+}
+
+echo "Activating feature 'rad'"
+
+version
+download
+install
