844bd3921b049abe869ae2dff9ed5b10be65c2f9
Lyon Till <158992+ljtill@users.noreply.github.com>
2024-01-28 22:18:54 +0000

    feat: add bindgen tool (#98)

---
+++ test/bindgen/version.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+# Import test library
+source dev-container-features-test-lib
+
+# Check the user
+check "vscode" whoami | grep vscode
+
+# Options-specific tests
+check "version" wit-bindgen --version | grep "0.16.0"
+
+# Report result
+reportResults
