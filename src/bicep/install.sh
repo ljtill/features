c7a55e1fd21625ca7e58bff919c9b83243a130c2
Lyon Till <158992+ljtill@users.noreply.github.com>
2024-01-30 12:02:22 +0000

    feat: add bicep tool (#105)

---
+++ src/bicep/install.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+set -e
+
+cd "$(mktemp -d)"
+
+check() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+            echo "Running apt update..."
+            apt update -y
+        fi
+        apt -y install --no-install-recommends "$@"
+    fi
+}
+
+export DEBIAN_FRONTEND=noninteractive
+
+check curl ca-certificates jq
+
+version() {
+    if [ "${VERSION}" = "latest" ]; then
+        export VERSION=$(curl -sL https://api.github.com/repos/azure/bicep/releases/latest | jq -r ".tag_name" | sed 's/v//')
+    else
+        export VERSION=$(echo ${VERSION} | sed 's/v//')
+    fi
+}
+
+download() {
+    curl -Lo ./bicep https://github.com/azure/bicep/releases/download/v"${VERSION}"/bicep-linux-x64
+}
+
+install() {
+    chmod +x ./bicep
+    chown root:root ./bicep
+    mv ./bicep /usr/local/bin/bicep
+}
+
+echo "Activating feature 'bicep'"
+
+version
+download
+install
