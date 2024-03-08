6285cfe421ca09c72bc1efcdd6b0b41cf21fa761
Lyon Till <lyon.till@microsoft.com>
2024-03-08 17:41:56 +0000

    feat: add binaryen tool

--- test/binaryen/version.sh
+++ test/binaryen/version.sh
@@ -9,19 +9,19 @@ source dev-container-features-test-lib
 check "vscode" whoami | grep vscode
 
 # Options-specific tests
-check "wasm2js" --version | grep "version_117"
-check "wasm-as" --version | grep "version_117"
-check "wasm-ctor-eval" --version | grep "version_117"
-check "wasm-dis" --version | grep "version_117"
-check "wasm-emscripten-finalize" --version | grep "version_117"
-check "wasm-fuzz-lattices" --version | grep "version_117"
-check "wasm-fuzz-types" --version | grep "version_117"
-check "wasm-merge" --version | grep "version_117"
-check "wasm-metadce" --version | grep "version_117"
-check "wasm-opt" --version | grep "version_117"
-check "wasm-reduce" --version | grep "version_117"
-check "wasm-shell" --version | grep "version_117"
-check "wasm-split" --version | grep "version_117"
+check "wasm2js" wasm2js --version | grep "version_117"
+check "wasm-as" wasm-as --version | grep "version_117"
+check "wasm-ctor-eval" wasm-ctor-eval --version | grep "version_117"
+check "wasm-dis" wasm-dis --version | grep "version_117"
+check "wasm-emscripten-finalize" wasm-emscripten-finalize --version | grep "version_117"
+check "wasm-fuzz-lattices" wasm-fuzz-lattices --version | grep "version_117"
+check "wasm-fuzz-types" wasm-fuzz-types --version | grep "version_117"
+check "wasm-merge" wasm-merge --version | grep "version_117"
+check "wasm-metadce" wasm-metadce --version | grep "version_117"
+check "wasm-opt" wasm-opt --version | grep "version_117"
+check "wasm-reduce" wasm-reduce --version | grep "version_117"
+check "wasm-shell" wasm-shell --version | grep "version_117"
+check "wasm-split" wasm-split --version | grep "version_117"
 
 # Report result
 reportResults
