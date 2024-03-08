83f4c5711787b552276edcaf057759671af2ea68
Lyon Till <lyon.till@microsoft.com>
2024-03-08 16:04:09 +0000

    feat: add binaryen tool

--- test/binaryen/test.sh
+++ test/binaryen/test.sh
@@ -6,19 +6,19 @@ set -e
 source dev-container-features-test-lib
 
 # Feature-specific tests
-check "wasm2js" --version
-check "wasm-as" --version
-check "wasm-ctor-eval" --version
-check "wasm-dis" --version
-check "wasm-emscripten-finalize" --version
-check "wasm-fuzz-lattices" --version
-check "wasm-fuzz-types" --version
-check "wasm-merge" --version
-check "wasm-metadce" --version
-check "wasm-opt" --version
-check "wasm-reduce" --version
-check "wasm-shell" --version
-check "wasm-split" --version
+check "wasm2js" wasm2js --version
+check "wasm-as" wasm-as --version
+check "wasm-ctor-eval" wasm-ctor-eval --version
+check "wasm-dis" wasm-dis --version
+check "wasm-emscripten-finalize" wasm-emscripten-finalize --version
+check "wasm-fuzz-lattices" wasm-fuzz-lattices --version
+check "wasm-fuzz-types" wasm-fuzz-types --version
+check "wasm-merge" wasm-merge --version
+check "wasm-metadce" wasm-metadce --version
+check "wasm-opt" wasm-opt --version
+check "wasm-reduce" wasm-reduce --version
+check "wasm-shell" wasm-shell --version
+check "wasm-split" wasm-split --version
 
 # Report result
 reportResults
