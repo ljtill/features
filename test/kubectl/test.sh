afea6107f02061fc79ca8f0953379ae28ae69ca7
Lyon Till <158992+ljtill@users.noreply.github.com>
2024-08-06 00:36:33 +0100

    chore: remove kubectl options (#144)

--- test/kubectl/test.sh
+++ test/kubectl/test.sh
@@ -8,5 +8,8 @@ source dev-container-features-test-lib
 # Feature-specific tests
 check "version" kubectl version --client=true --output=json
 
+# Options-specific tests
+check "kubelogin" kubelogin --version
+
 # Report result
 reportResults
