fd58908a71cb3aaaa385dcd0d612109d4f0beeff
Lyon Till <158992+ljtill@users.noreply.github.com>
2024-08-06 23:52:40 +0000

    chore: handle http failure

--- src/argocd/install.sh
+++ src/argocd/install.sh
@@ -19,7 +19,12 @@ check curl ca-certificates jq
 
 version() {
     if [ "${VERSION}" = "latest" ]; then
-        export VERSION=$(curl -sLf https://api.github.com/repos/argoproj/argo-cd/releases/latest | jq -r ".tag_name" | sed 's/v//')
+        URL="https://api.github.com/repos/argoproj/argo-cd/releases/latest"
+        if ! curl -sLf -o ./response.json "$URL"; then
+            echo "ERROR: Unable to fetch latest version"
+            exit 1
+        fi
+        export VERSION=$(cat ./response.json | jq -r ".tag_name" | sed 's/v//')
     else
         export VERSION=$(echo ${VERSION} | sed 's/v//')
     fi
