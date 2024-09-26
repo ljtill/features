ab9adcb48432b1f17ad1f6d5958b195063f370a3
Lyon Till <lyon.till@microsoft.com>
2024-09-26 10:38:40 +0100

    fix: add task extension

--- src/task/install.sh
+++ src/task/install.sh
@@ -19,14 +19,23 @@ check curl ca-certificates jq
 
 version() {
     if [ "${VERSION}" = "latest" ]; then
-        export VERSION=$(curl -sL https://api.github.com/repos/go-task/task/releases/latest | jq -r ".tag_name" | cut -d'-' -f 4 | sed 's/v//')
+        URL="https://api.github.com/repos/go-task/task/releases/latest"
+        if ! curl -sLf -o ./response.json "$URL"; then
+            echo "ERROR: Unable to fetch latest version"
+            exit 1
+        fi
+        export VERSION=$(cat ./response.json | jq -r ".tag_name" | sed 's/v//')
     else
         export VERSION=$(echo ${VERSION} | sed 's/v//')
     fi
 }
 
 download() {
-    curl -Lo ./task_linux_amd64.tar.gz https://github.com/go-task/task/releases/download/v"${VERSION}"/task_linux_amd64.tar.gz
+    URL="https://github.com/go-task/task/releases/download/v"${VERSION}"/task_linux_amd64.tar.gz"
+    if ! curl -sLf -o ./task_linux_amd64.tar.gz "$URL"; then
+        echo "ERROR: Unable to download file"
+        exit 1
+    fi
 }
 
 install() {
