95cd0d82784a297da1869455b53d447bf31b461b
Lyon Till <lyon.till@microsoft.com>
2024-09-26 14:59:41 +0100

    feat: add mkcert tool

---
+++ test/mkcert/version.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+# Import test library
+source dev-container-features-test-lib
+
+# Check the user
+check "vscode" whoami | grep vscode
+
+# Options-specific tests
+check "version" mkcert --version | grep "1.4.4"
+
+# Report result
+reportResults
