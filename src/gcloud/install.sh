8c54f590708363817b4cac77abe21fef6ef25e35
Lyon Till <158992+ljtill@users.noreply.github.com>
2024-10-22 18:26:27 +0200

    feat: add gcloud tool (#159)

---
+++ src/gcloud/install.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+set -e
+
+cd "$(mktemp -d)"
+
+check() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+            echo "Running apt update..."
+            apt update -y
+        fi
+        apt -y install --no-install-recommends "$@"
+    fi
+}
+
+export DEBIAN_FRONTEND=noninteractive
+
+check curl ca-certificates jq
+
+version() {
+    if [ "${VERSION}" = "latest" ]; then
+        export URL="https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-linux-x86_64.tar.gz"
+    else
+        VERSION=$(echo ${VERSION} | sed 's/v//')
+        export URL="https://storage.googleapis.com/cloud-sdk-release/google-cloud-cli-"${VERSION}"-linux-x86_64.tar.gz"
+    fi
+}
+
+download() {
+    if ! curl -sLf -o ./google-cloud-cli-linux-x86_64.tar.gz "$URL"; then
+        echo "ERROR: Unable to download file"
+        exit 1
+    fi
+}
+
+install() {
+    tar -zxof ./google-cloud-cli-linux-x86_64.tar.gz
+    mv ./google-cloud-sdk /opt/
+    /opt/google-cloud-sdk/install.sh --rc-path /etc/bash.bashrc --quiet
+}
+
+echo "Activating feature 'gcloud'"
+
+version
+download
+install
---
+++ test/gcloud/version.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+# Import test library
+source dev-container-features-test-lib
+
+# Check the user
+check "vscode" whoami | grep vscode
+
+# Options-specific tests
+check "version" gcloud version | grep "498.0.0"
+
+# Report result
+reportResults
