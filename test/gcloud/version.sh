ebc6d18d2dcd12ed41dc49c2eb2b644177fbf518
Lyon Till <158992+ljtill@users.noreply.github.com>
2025-01-16 14:12:52 +0000

    feat: add component update

--- test/gcloud/version.sh
+++ test/gcloud/version.sh
@@ -9,7 +9,7 @@ source dev-container-features-test-lib
 check "vscode" whoami | grep vscode
 
 # Options-specific tests
-check "version" gcloud version | grep "498.0.0"
+check "version" gcloud version | grep "505.0.0"
 
 # Report result
 reportResults
