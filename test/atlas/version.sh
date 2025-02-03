c732ee82485d709f0d66c14a04b0c05856a25901
Lyon Till <158992+ljtill@users.noreply.github.com>
2025-02-03 15:17:39 +0000

    feat: add atlas tool (#175)

---
+++ test/atlas/version.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+# Import test library
+source dev-container-features-test-lib
+
+# Check the user
+check "vscode" whoami | grep vscode
+
+# Options-specific tests
+check "version" atlas --version | grep "1.37.0"
+
+# Report result
+reportResults
