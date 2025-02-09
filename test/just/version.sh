a718fb100b0389a315c5bb9a3613030a9af1a346
Lyon Till <158992+ljtill@users.noreply.github.com>
2025-02-09 14:03:08 +0000

    feat: add just tool

---
+++ test/just/version.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+# Import test library
+source dev-container-features-test-lib
+
+# Check the user
+check "whoami" whoami | grep vscode
+
+# Options-specific tests
+check "version" just --version | grep "1.39.0"
+
+# Report result
+reportResults
