2f94b99ca19b795e75e410529f85c8d2b39df182
Lyon Till <158992+ljtill@users.noreply.github.com>
2025-03-10 11:05:37 +0000

    feat: add volta tool (#214)

---
+++ test/volta/version.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+# Import test library
+source dev-container-features-test-lib
+
+# Check the user
+check "whoami" whoami | grep vscode
+
+# Options-specific tests
+check "version" volta --version | grep "2.0.2"
+
+# Report result
+reportResults
